69a1c3bc931a1d65487d649f9fa7f8bbb2f2af6c
Sahil <sahilofficial17@gmail.com>
2024-09-24 22:30:27 -0700

    Created UI to take user query

--- geoai/main.py
+++ geoai/main.py
@@ -1,13 +1,15 @@
 from agents.main_agent import MainAgent
+import streamlit as st
 
 def main():
-    user_query = "Identify all swimming pools in Los Angeles and provide me with a map and summary report."
+    st.title("Foundation Model Analysis Test")
+    user_query = st.text_input("Enter your query here", "Identify all swimming pools in Los Angeles and provide me with a map and summary report.")
+    # user_query = "Identify all swimming pools in Los Angeles and provide me with a map and summary report."
 
-    agent = MainAgent()
-
-    result = agent.run(user_query)
-
-    print(result)
+    if st.button("Run Query"):
+        agent = MainAgent()
+        result = agent.run(user_query)
+        st.write(result)
 
 if __name__ == "__main__":
     main()
